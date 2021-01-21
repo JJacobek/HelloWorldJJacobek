434d2b1751d8caa857d07e3891b29fbd5a615702
Justin Jacobek <jjacobek@usf.edu>
2021-01-21 13:48:44 -0500

    version 2

--- HelloWorldJJacobek/Program.cs
+++ HelloWorldJJacobek/Program.cs
@@ -6,7 +6,9 @@ namespace HelloWorldJJacobek
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
